cb9822b78100f951967aa0874bca532aa0978482
Johnson0213 <johnson0213.am07@nctu.edu.tw>
2021-12-06 04:46:10 +0800

    add python version of code for unittest

---
+++ python/layers/debug.py
@@ -0,0 +1,5 @@
+dbg_msg=False
+
+def d_print(*args):
+    if dbg_msg:
+        print(*args)
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    int aa = 909;
+    const int a;
+    printf("aa: %d", &aa);
+}
